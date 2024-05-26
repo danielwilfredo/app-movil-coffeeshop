2ba7aa4388fb84836ac811370dc2874c23b49634
Wilfredo <xvwilito11@gmail.com>
2024-05-25 23:07:14 -0600

    trabajando funcionalidad de detalle del carrito

---
+++ src/components/CarritoCard/CarritoCard.js
@@ -0,0 +1,122 @@
+import React, { useEffect, useState } from 'react';
+import { Text, TouchableOpacity, View, StyleSheet, FlatList, Alert } from 'react-native';
+import Constants from 'expo-constants';
+import * as Constantes from '../../utils/constantes'
+
+const CarritoCard = ({item, cargarCategorias}) => {
+
+    const ip = Constantes.IP;
+
+    const handleDeleteDetalleCarrito = async (idDetalle) => {
+        // Lógica para agregar al carrito con la cantidad ingresada
+        try {
+          const formData = new FormData();
+          formData.append('idDetalle', idDetalle);
+          const response = await fetch(`${ip}/coffeeshop/api/services/public/pedido.php?action=deleteDetail`, {
+            method: 'POST',
+            body: formData
+        });
+        const data = await response.json();
+        if (data.status) {
+            Alert.alert('Datos elimnados correctamente del carrito');
+            cargarCategorias();
+        } else {
+            Alert.alert('Error al agregar al carrito', data.error);
+        }
+        } catch (error) {
+        Alert.alert("Error en agregar al carrito")
+        }
+      };
+    
+
+  return (
+    <View style={styles.itemContainer}>
+    <Text style={styles.itemText}>ID: {item.id_detalle}</Text>
+    <Text style={styles.itemText}>Nombre: {item.nombre_producto}</Text>
+    <Text style={styles.itemText}>Precio: ${item.precio_producto}</Text>
+    <Text style={styles.itemText}>Cantidad: {item.cantidad_producto}</Text>
+    <TouchableOpacity style={styles.modifyButton}>
+      <Text style={styles.buttonText}>Modificar Cantidad</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.deleteButton}
+    onLongPress={()=>handleDeleteDetalleCarrito(item.id_detalle)}
+    >
+      <Text style={styles.buttonText}>Elimina</Text>
+    </TouchableOpacity>
+  </View>
+
+  );
+};
+
+export default CarritoCard;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EAD8C0',
+    paddingTop: Constants.statusBarHeight,
+    paddingHorizontal: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 16,
+    color: '#5C3D2E', // Brown color for the title
+  },
+  itemContainer: {
+    padding: 16,
+    marginVertical: 8,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  itemText: {
+    fontSize: 16,
+    marginBottom: 4,
+    color: '#333',
+  },
+  modifyButton: {
+    borderWidth: 1,
+    borderColor: '#8F6B58',
+    borderRadius: 8,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: '#8F6B58', // Light brown color for modify button
+    marginVertical: 4,
+  },
+  deleteButton: {
+    borderWidth: 1,
+    borderColor: '#D2691E',
+    borderRadius: 8,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: '#D2691E', // Darker orange color for delete button
+    marginVertical: 4,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 16,
+  },
+  finalButton: {
+    backgroundColor: '#A0522D', // Sienna color for final action buttons
+    padding: 16,
+    borderRadius: 8,
+    marginVertical: 8,
+  },
+  finalButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  containerButtons:{
+    justifyContent:'center',
+    alignItems:'center', 
+  }
+});
--- src/screens/Carrito.js
+++ src/screens/Carrito.js
@@ -1,45 +1,140 @@
-import {useState} from 'react'
-import {Text, Touchable, TouchableOpacity, View, StyleSheet} from 'react-native'
+import React, { useEffect, useState } from 'react';
+import { Text, TouchableOpacity, View, StyleSheet, FlatList, Alert } from 'react-native';
 import Constants from 'expo-constants';
+import * as Constantes from '../utils/constantes';
+import Buttons from '../components/Buttons/Button';
+import CarritoCard from '../components/CarritoCard/CarritoCard';
 
+const Carrito = ({ navigation }) => {
+  const [dataDetalleCarrito, setDataDetalleCarrito] = useState([]);
+  const ip = Constantes.IP;
 
-const Carrito = ({navigation}) => {
+  const backProducts = () => {
+    navigation.navigate('Productos');
+  };
 
-    const backProducts = () =>{
-        navigation.navigate('Productos')
+  useEffect(() => {
+    getDetalleCarrito();
+  }, []);
+
+  const getDetalleCarrito = async () => {
+    try {
+      const response = await fetch(`${ip}/coffeeshop/api/services/public/pedido.php?action=readDetail`, {
+        method: 'GET',
+      });
+
+      const data = await response.json();
+      console.log("data al obtener detalle carrito  \n", data);
+      if (data.status) {
+        setDataDetalleCarrito(data.dataset);
+      } else {
+        console.log(data);
+        Alert.alert('Error', data.error);
+      }
+    } catch (error) {
+      console.error(error, "Error desde Catch");
+      Alert.alert('Error', 'Ocurrió un error al listar las categorias');
     }
+  };
 
-    return (  
-<View style={styles.container}>
-<Text>
-    Aqui se mostrarán los productos
-</Text>
+  const renderItem = ({ item }) => (
+    <CarritoCard item={item} cargarCategorias={getDetalleCarrito} />
+  );
 
-<TouchableOpacity
-onPress={backProducts}>
-    <Text>Regresar a productos</Text>
-</TouchableOpacity>
-</View>
-    );
-}
- 
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Carrito de Compras</Text>
+      {dataDetalleCarrito.length > 0 ? (
+        <FlatList
+          data={dataDetalleCarrito}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id_detalle.toString()}
+        />
+      ) : (
+        <Text>No hay detalles del carrito disponibles.</Text>
+      )}
+      <View style={styles.containerButtons}>
+        <Buttons
+          textoBoton='Finalizar Pedido'
+          accionBoton={backProducts} />
+        <Buttons
+          textoBoton='Regresar a productos'
+          accionBoton={backProducts} />
+      </View>
+
+    </View>
+  );
+};
+
 export default Carrito;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#EAD8C0',
-      paddingTop: Constants.statusBarHeight,
-    },
-    button: {
-      borderWidth: 2,
-      borderColor: "black",
-      width: 100,
-      borderRadius: 10,
-      backgroundColor: "darkblue"
-    },
-    buttonText: {
-      textAlign: 'center',
-      color: "white"
-    }
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#EAD8C0',
+    paddingTop: Constants.statusBarHeight,
+    paddingHorizontal: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 16,
+    color: '#5C3D2E', // Brown color for the title
+  },
+  itemContainer: {
+    padding: 16,
+    marginVertical: 8,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  itemText: {
+    fontSize: 16,
+    marginBottom: 4,
+    color: '#333',
+  },
+  modifyButton: {
+    borderWidth: 1,
+    borderColor: '#8F6B58',
+    borderRadius: 8,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: '#8F6B58', // Light brown color for modify button
+    marginVertical: 4,
+  },
+  deleteButton: {
+    borderWidth: 1,
+    borderColor: '#D2691E',
+    borderRadius: 8,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: '#D2691E', // Darker orange color for delete button
+    marginVertical: 4,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 16,
+  },
+  finalButton: {
+    backgroundColor: '#A0522D', // Sienna color for final action buttons
+    padding: 16,
+    borderRadius: 8,
+    marginVertical: 8,
+  },
+  finalButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  containerButtons: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
