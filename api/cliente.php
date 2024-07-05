e2447733e278ba6f0375ce3a068a68dc82a98c9c
Wilfredo <xvwilito11@gmail.com>
2024-07-04 21:34:02 -0600

    comentarios

--- api/cliente.php
+++ api/cliente.php
@@ -62,11 +62,8 @@ if (isset($_GET['action'])) {
                 if (!$captcha['success']) {
                     $result['recaptcha'] = 1;
                     $result['error'] = 'No eres humano';
-<<<<<<< HEAD
                 } elseif(false) {
-=======
                 } elseif(!isset($_POST['condicion'])) {
->>>>>>> 3ae0e6c003b94c836b0ad702f41153f9aed8c00a
                     $result['error'] = 'Debe marcar la aceptación de términos y condiciones';
                 } elseif (
                     !$cliente->setNombre($_POST['nombreCliente']) or
@@ -134,6 +131,9 @@ if (isset($_GET['action'])) {
 } else {
     print(json_encode('Recurso no disponible'));
 }
+
+
+// OJO CON ESTA PARTE, ESTE CODIGO VA EN EL ARCHIVO DE LA CLASE CLIENTE_HANDLER.PHP 
 
 //Agregar esta función en el archivo handler.php de clientes
 
