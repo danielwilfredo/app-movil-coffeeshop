39769b12d4fac5a7b36f84c6432e198a0daab510
Wilfredo <xvwilito11@gmail.com>
2024-08-21 19:27:58 -0600

    actualizando repo desde casa

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -2,8 +2,7 @@
 //IP de donde se encuentra la Api, en este caso la dirección 
 //Ip de la maquina local 
 //utilizar la direccion IP del servidor y no localhost
-//export const IP = 'http://10.10.0.165';
+export const IP = 'https://www.wilfredo.site/';
 //export const IP = 'http://192.168.137.65';
-export const IP = 'http://192.168.1.2';
-//export const IP = 'http://172.20.10.10';
+
 
